af6cc4213a7950e7f3d53443a9d2eef208e5053b
pragyanpath-official1 <shuklaraja99056@gmail.com>
2026-01-11 10:27:55 +0530

    Update sw.js

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -3,27 +3,21 @@ const CACHE_NAME = "pragyanpath-v1";
 const urlsToCache = [
   "/",
   "/index.html",
+  "/assets/manifest.json",
   "/assets/css/style.css",
   "/assets/js/chess.js",
-  "/assets/images/logo.png",
-  "/assets/images/icon-192.png",
-  "/assets/images/icon-512.png"
+  "/assets/icons/icon-192.png",
+  "/assets/icons/icon-512.png"
 ];
 
-// Install
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
